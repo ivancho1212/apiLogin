2d6c8adcc3c282527da8890fd00e755a1e379210
Iván Herrera <119714354+ivancho1212@users.noreply.github.com>
2024-04-03 22:02:13 -0500

    finish login for firebase

--- login_firebase/assets/js/app/firebase.js
+++ login_firebase/assets/js/app/firebase.js
@@ -1,6 +1,6 @@
 // Import the funtions  you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +19,8 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+// Import the functions you need from the SDKs you need
+
+
+
+
